a0a509f846f1cf92174d8001a5a887d30deeeb6f
jpdiogoNOVA <jp.diogo@campus.fct.unl.pt>
2021-02-18 21:37:50 +0000

    LinkNap base v0

--- assets/js/ecorep.js
+++
@@ -1,5 +0,0 @@
-
-var logged = getLogged();
-var points = document.getElementById("ecoPoints");
-points.innerHTML = logged.points;
-
--- assets/js/navbar.js
+++
@@ -1,8 +0,0 @@
-$(".sidebar").hover(function(){
-   $(".wrapper").toggleClass("collapser");
-});
-
-var logged = getLogged();
-username = document.getElementById("header_username");
-if(username != null)
-   username.innerHTML = logged.firstname + " " + logged.surname;
